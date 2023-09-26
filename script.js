52d345523b62b3d79dadf2c09c39c906442832ea
Vitorio Tuckers <vitoriofibla@hotmail.com>
2023-09-26 12:12:37 -0300

    Changelog -Added checkWin function to handle win condition for both players -Added gameFlow function to handle UI updates and restarting the game when it ends. -Improved code encapsulation

--- script.js
+++ script.js
@@ -2,25 +2,65 @@ const gameBoardElement = document.querySelector('#gameboard');
 const clearButton = document.querySelector('#clear');
 
 const Gameboard = (() => {
-  let currentPlayer = 'player';
   let gameBoard = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ];
 
-  const markSquare = (row, collumn) => {
-    if (currentPlayer === 'player') {
-      gameBoard[row][collumn] = 'X';
-      currentPlayer = 'cpu';
-    } else {
-      gameBoard[row][collumn] = 'O';
-      currentPlayer = 'player';
-    }
-    displayBoard();
+  const restartGame = () => {
+    gameBoard = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+    updateDisplay();
   };
 
-  const displayBoard = () => {
+  const checkWin = player => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        (gameBoard[i][0] === player &&
+          gameBoard[i][1] === player &&
+          gameBoard[i][2] === player) ||
+        (gameBoard[0][i] === player &&
+          gameBoard[1][i] === player &&
+          gameBoard[2][i] === player)
+      ) {
+        return true;
+      }
+    }
+
+    if (
+      (gameBoard[0][0] === player &&
+        gameBoard[1][1] === player &&
+        gameBoard[2][2] === player) ||
+      (gameBoard[0][2] === player &&
+        gameBoard[1][1] === player &&
+        gameBoard[2][0] === player)
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const gameFlow = () => {
+    updateDisplay();
+    if (checkWin('X')) {
+      setTimeout(() => {
+        alert('Player X wins');
+        restartGame();
+      }, 1000);
+    } else if (checkWin('O')) {
+      setTimeout(() => {
+        alert('Player O wins');
+        restartGame();
+      }, 1000);
+    }
+  };
+
+  const updateDisplay = () => {
     for (let row = 0; row < 3; row++) {
       for (let col = 0; col < 3; col++) {
         const square = gameBoardElement.querySelector(
@@ -31,18 +71,13 @@ const Gameboard = (() => {
     }
   };
 
-  const clearBoard = () => {
-    gameBoard = [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', ''],
-    ];
-    displayBoard();
+  const markSquare = (row, collumn) => {
+    gameBoard[row][collumn] = 'X';
+    gameFlow();
   };
 
   return {
     markSquare,
-    clearBoard,
   };
 })();
 
@@ -51,8 +86,4 @@ gameBoardElement.addEventListener('click', e => {
   let row = Number(clickTarget[0]);
   let collumn = Number(clickTarget[1]);
   Gameboard.markSquare(row, collumn);
-});
-
-clearButton.addEventListener('click', () => {
-  Gameboard.clearBoard();
 });
